239211c40297332d2d48960dce5204c580edc306
AnabelMarinho <anabel.soares2001@gmail.com>
2026-05-19 19:44:23 -0300

    :lipstick: feat: configura estilos globais, tipografia e imagens

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -6,11 +8,17 @@ export const metadata: Metadata = {
   description: 'Sistema de Gestão da PROPPG',
 };
 
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+  variable: '--font-roboto',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={roboto.variable}>
       <body>{children}</body>
     </html>
   );
