24c60d3a37d026406147489ab5bc139d7c4cc99d
AnabelMarinho <anabel.soares2001@gmail.com>
2026-05-20 10:18:54 -0300

    :sparkles: feat: implementa tela de esqueceu senha

--- frontend/src/app/(public)/esqueceu-senha/page.tsx
+++ frontend/src/app/(public)/esqueceu-senha/page.tsx
@@ -1,9 +1,94 @@
 'use client';
 
-export default function EsqueceuSenhaPage() {
+import { useState } from 'react';
+import Link from 'next/link';
 
-    return(
-        <p>Tela de esqueceu senha criada</p>
-    )
-   
+import { Alert, Typography } from '@mui/material';
+
+import AuthCard from '@/components/auth/AuthCard';
+import AuthDivider from '@/components/auth/AuthDivider';
+import AuthLayout from '@/components/auth/AuthLayout';
+
+import AppButton from '@/components/ui/AppButton';
+import AppInput from '@/components/ui/AppInput';
+
+export default function EsqueceuSenhaPage() {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [emailSent, setEmailSent] = useState(false);
+
+  function validateEmail(email: string) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  function handleSubmit() {
+    const isValid = validateEmail(email);
+
+    if (!isValid) {
+      setEmailError(true);
+      setEmailSent(false);
+
+      return;
+    }
+
+    setEmailError(false);
+
+    // futuramente aqui vai chamada da API
+    setEmailSent(true);
+  }
+
+  return (
+    <AuthLayout imageSrc="/esqueceu-senha.svg">
+      <AuthCard title="Recuperar senha">
+        <Typography
+          sx={{
+            textAlign: 'center',
+            fontSize: 'var(--font-xs)',
+            lineHeight: 1.5,
+            color: 'var(--medium-gray)',
+            mb: '2rem',
+          }}
+        >
+          Insira o endereço de e-mail registrado com a sua conta e selecione
+          &quot;Enviar&quot;. Um e-mail será enviado ao seu endereço com um link
+          para renovar a sua senha.
+        </Typography>
+
+        {emailSent && (
+          <Alert severity="success" sx={{ mb: '2rem' }}>
+            Link de recuperação enviado com sucesso.
+          </Alert>
+        )}
+
+        <AppInput
+          label="E-mail*"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          error={emailError}
+          helperText={emailError ? 'Digite um e-mail válido.' : ''}
+        />
+
+        <AppButton sx={{ mt: '4rem' }} onClick={handleSubmit}>
+          Enviar
+        </AppButton>
+
+        <AuthDivider />
+
+        <Typography
+          component={Link}
+          href="/login"
+          sx={{
+            display: 'block',
+            textAlign: 'center',
+            fontSize: 'var(--font-sm)',
+            color: 'var(--muted-text-color)',
+            '&:hover': { textDecoration: 'underline' },
+          }}
+        >
+          Voltar para o login
+        </Typography>
+      </AuthCard>
+    </AuthLayout>
+  );
 }
